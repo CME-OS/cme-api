e122a00bd4e484941ced0e70ddb77744818faf6b
Oke Ugwu <oke.ugwu@justdevelop.it>
2015-05-22 13:19:36 +0100

    added analytics endpoint

---
+++ src/Controllers/Analytics/GetLastEvents.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @author  oke.ugwu
+ */
+namespace CmeApi\Controllers\Analytics;
+
+use CmeApi\AbstractController;
+use CmeKernel\Core\CmeKernel;
+use CmeKernel\Enums\EventType;
+use Slim\Http\Request;
+
+class GetLastEvents extends AbstractController
+{
+
+  public function _process(Request $request)
+  {
+    $result['status']  = 'success';
+    $result['result']  = null;
+    $result['request'] = $request->post();
+    try
+    {
+      $eventType                   = $request->post('event_type');
+      $result['data']['campaigns'] = CmeKernel::Analytics()->getLastXOfEvent(
+        EventType::fromValue($eventType),
+        $request->post('campaign_id')
+      );
+      $result['message']           = $eventType . ' Events returned.';
+    }
+    catch(\Exception $e)
+    {
+      $result['status'] = 'fail';
+      $result['error']  = $e->getMessage();
+    }
+
+    return $result;
+  }
+}
